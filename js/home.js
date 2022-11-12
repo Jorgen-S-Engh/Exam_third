bf54b58e5de790de94e6e8d5a2d1e0abaa74720a
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-11-12 20:14:33 +0100

    fix profile img if missing

--- js/home.js
+++ js/home.js
@@ -1,2 +1,28 @@
-const profileImg = document.querySelector(".profile-img-small").src = localStorage.getItem("avatar")
+
+const profileName = document.querySelector(".header-name").innerHTML = `${localStorage.getItem("name")}`
+const profileImg = document.querySelector(".profile-img-small")
+
+async function setImg () {
+    if (localStorage.getItem("avatar") !== ""){
+        profileImg.src = localStorage.getItem("avatar");
+        console.log("inside if")
+    }else{
+        profileImg.src = "img/user-profile.png"
+    } 
+}
+
+setImg();
+
+
+console.log(localStorage.getItem("avatar") !== "")
+
+
+
+// profileImg.addEventListener("error", () =>{
+//   const defaultImage =
+//     "img/user-profile.png";
+
+//   img.src = defaultImage;
+//   img.alt = 'default';
+// });
 
